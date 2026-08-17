e877bc666e3e9ccc6602167c5be471ae5158044d
LuisG-santos <luisgustavo.nunes@icloud.com>
2026-08-17 03:27:10 -0300

    feat: update favicon and header logo for improved branding

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,10 @@
-import { PackageOpen } from "lucide-react"
+import logo from "@/assets/icon.png"
 
 export function Header() {
     return (
         <>
-            <div className="flex bg-zinc-900 border-b-1 space-x-3 pl-10 pt-2 h-15">
-                <PackageOpen className="w-10 h-10 text-white" />
+            <div className="flex bg-zinc-900 items-center border-b-1 pl-10 gap-3 pt-2 h-15">
+                <img src={logo} alt="Candidate Processor" className="w-auto h-10 rounded-xl"></img>
                 <div>
                     <h1 className="font-semibold">Candidate Processor</h1>
                     <p className="text-sm text-zinc-500">BTG</p>
